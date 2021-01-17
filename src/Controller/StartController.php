2b17fcb2244e87d635853a05e835eee2e5b463aa
Elma <6303@htl.rennweg.at>
2021-01-17 10:04:54 +0100

    Basic Javascript and CSS implementation

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -17,21 +17,25 @@ class StartController extends AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/",name="app_homepage")
      */
     public function homepage(){
-        return new Response('hallo');
+        return $this->render('start/homepage.html.twig');
     }
 
 
+
     /**
-     * @Route("/start/{slug}")
+     * @Route("/start/{slug}",name="app_start_show")
      */
     public function show($slug){
 
         $answers= [
             'a', 'b', 'c'
             ];
+
+
+        dump($this);
 
         return $this->render('start/show.html.twig',[
             'start' => ucwords(str_replace("-"," ",$slug)),
