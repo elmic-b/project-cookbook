354060416cefc09fc0e4e3abf1b3f2139afb8f2c
Elma <6303@htl.rennweg.at>
2021-01-17 12:18:11 +0100

    Markdown tutorial and Services

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -9,9 +9,12 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Twig\Environment;
 
 class StartController extends AbstractController
 {
@@ -19,7 +22,7 @@ class StartController extends AbstractController
     /**
      * @Route("/",name="app_homepage")
      */
-    public function homepage(){
+    public function homepage( Environment $twigEnvironment){
         return $this->render('start/homepage.html.twig');
     }
 
@@ -28,17 +31,23 @@ class StartController extends AbstractController
     /**
      * @Route("/start/{slug}",name="app_start_show")
      */
-    public function show($slug){
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache){
 
         $answers= [
-            'a', 'b', 'c'
+            'abc `tics` lolo' , 'b', 'c'
             ];
 
+        $questionText='I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function () use ($questionText, $markdownParser){
+
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
         dump($this);
 
         return $this->render('start/show.html.twig',[
             'start' => ucwords(str_replace("-"," ",$slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers
         ]);
 
